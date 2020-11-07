59198fd33ce5d29118d2978d9861bf0543eeba07
Ebrahim Mo Gedamy <hemagedamy@gmail.com>
2020-11-07 19:17:21 +0200

    Photo slider

---
+++ PhotoSlider/PhotoSliderViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  PhotoSlider
+//
+//  Created by Ebrahim  Mo Gedamy on 11/7/20.
+//
+
+import UIKit
+
+// MARK: PhotoSliderViewController
+class PhotoSliderViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var photoSliderView: PhotoSliderView!
+    
+    // MARK: View Methods
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let images: [UIImage] = [UIImage(named: "image1")!,
+                                 UIImage(named: "image2")!,
+                                 UIImage(named: "image3")!,
+                                 UIImage(named: "image1")!,
+                                 UIImage(named: "image2")!,
+                                 UIImage(named: "image3")!]
+        
+        photoSliderView.configure(with: images)
+    }
+}
+
